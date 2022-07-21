315e90fe6b9b7f8277e955e0504ec639e9458c4e
neuralworm <nerualworm@protonmail.com>
2022-07-20 17:07:09 -0700

    File name now grabbed form context menu when selecting import defualt option.

--- src/disposables/importDefault.ts
+++ src/disposables/importDefault.ts
@@ -2,12 +2,17 @@ import * as vscode from 'vscode';
 import getFileLineArray from './lib/getFile';
 import getLineToImportAt from './lib/getImportLine';
 
-export default vscode.commands.registerCommand('importer.importDefault', () => {
+export default vscode.commands.registerCommand('importer.importDefault', (uri: vscode.Uri) => {
     // The code you place here will be executed every time your command is executed
     // Display a message box to the user
+
+    // get file name without full path
+
+    console.log(uri.fsPath)
+    let fileName: string = uri.fsPath.split('\\')[uri.fsPath.split('\\').length - 1].split('.')[0]
     vscode.window.showInputBox({
         title: "Enter Import Name",
-        value: "FILE_NAME"
+        value: fileName
     }).then(()=>{
         vscode.window.showInformationMessage("IMPORTED")
         let fileArray: string[] = getFileLineArray(vscode.window.activeTextEditor!)
